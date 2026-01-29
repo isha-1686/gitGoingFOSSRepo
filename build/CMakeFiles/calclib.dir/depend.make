b7454009830f964a58dfe4442519696d6730b5e9
Shishir S Tambe <shishirstambe01@gmail.com>
2026-01-29 13:29:44 +0000

    docs: clarify CLI help text and add usage examples

---
+++ build/CMakeFiles/calclib.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for calclib.
+# This may be replaced when dependencies are built.
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/gitGoingFOSSRepo/tests
+# Build directory: /workspaces/gitGoingFOSSRepo/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(calc_tests "/workspaces/gitGoingFOSSRepo/build/tests/calc_tests")
+set_tests_properties(calc_tests PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/gitGoingFOSSRepo/tests/CMakeLists.txt;5;add_test;/workspaces/gitGoingFOSSRepo/tests/CMakeLists.txt;0;")
